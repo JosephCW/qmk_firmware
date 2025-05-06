0709d85f05439cc0a2f8903461941c82220f413f
Joseph Watts <josephcw@live.com>
2025-05-06 12:24:31 -0500

    simplify sweep mapping

--- keyboards/ferris/keymaps/joey/config.h
+++ keyboards/ferris/keymaps/joey/config.h
@@ -3,16 +3,16 @@
 #define SPLIT_USB_DETECT
 
 // Configure the global tapping term (default: 200ms)
-#define TAPPING_TERM 275
+#define TAPPING_TERM 225
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-#define QUICK_TAP_TERM 0
+#define QUICK_TAP_TERM 175
 
 // Leaders
-#define LEADER_TIMEOUT 300
-// Remove timout for first button press in leader
-#define LEADER_NO_TIMEOUT
-#define LEADER_PER_KEY_TIMING
+// #define LEADER_TIMEOUT 300
+// // Remove timout for first button press in leader
+// #define LEADER_NO_TIMEOUT
+// #define LEADER_PER_KEY_TIMING
 
 // Set combo timeout for esc, enter, tab, backspace
 #define COMBO_TERM 80
